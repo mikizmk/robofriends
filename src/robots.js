b37d7d1feb8b759c48c9ac134e58d11530952c7f
Mihajlo Zelenkovski <Miki@mihajlos-macbook-pro-2.home>
2021-10-12 22:20:19 +0200

    addin first files-1

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+{
+		id: 1,
+		name: 'Leame Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+},
+{
+		id: 2,
+		name: 'Ervin Howell',
+		username: 'Antoanette',
+		email: 'Shanna@lissa.tv'
+},
+{
+		id: 3,
+		name: 'Clementine Bauch',
+		username: 'Samantha',
+		email: 'Nathan@yesenia.net'
+},
+{
+		id: 4,
+		name: 'Patricia Lebsack',
+		username: 'Karianne',
+		email: 'Juliane.OConner@kery.org'
+},
+{
+		id: 5,
+		name: 'Chelsey Dietrich',
+		username: 'Kamren',
+		email: 'Lucio_Hettinger@annie.ca'
+},
+{
+		id: 6,
+		name: 'Mrs. Dennis Schulist',
+		username: 'Leopoldo_Corkery',
+		email: 'Karley_Dache@jasper.info'
+},
+{
+		id: 7,
+		name: 'Kurtis Weissnat',
+		username: 'Elwyn.Skiley',
+		email: 'Telly.Hoeger@billy.biz'
+},
+{
+		id: 8,
+		name: 'Nicholas Runolfsdottir',
+		username: 'Maxme_Nienow',
+		email: 'Sherwood@rosamond.me'
+},
+{
+		id: 9,
+		name: 'Glena Reichert',
+		username: 'Delphine',
+		email: 'Chaim_McDermont@dana.io'
+},
+{
+		id: 10,
+		name: 'Clementina DuBuque',
+		username: 'Moriah.Stanton',
+		email: 'Rey.Padberg@karina.biz'
+},
+
+]
